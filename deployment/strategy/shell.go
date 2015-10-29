e721e1d023e756f5aed800c112f596cb3472afa0
Matt Fellows <matt.fellows@onegeek.com.au>
2015-10-29 11:31:08 +1100

    Shell deployment strategy now runs shell scripts

--- deployment/strategy/shell.go
+++ deployment/strategy/shell.go
@@ -2,14 +2,18 @@ package strategy
 
 import (
 	"errors"
+	"fmt"
 	"github.com/mefellows/godspeed/log"
 	"github.com/mefellows/plugo/plugo"
 	"math/rand"
+	"os/exec"
 	"time"
 )
 
 type ShellDeploymentStrategy struct {
 	Host             string `mapstructure:"ssh_host"`
+	Shell            string
+	ShellArgs        []string `mapstructure:"shell_args"`
 	Commands         []string
 	RollbackCommands []string `mapstructure:"rollback_commands"`
 }
@@ -26,10 +30,31 @@ func (s ShellDeploymentStrategy) Setup() {
 	log.Debug("Setting up Shell")
 }
 
+func (s ShellDeploymentStrategy) createCommand(command string) *exec.Cmd {
+	// Create command string
+	args := append(s.ShellArgs, fmt.Sprintf(`"%s"`, command))
+	return exec.Command(s.Shell, args...)
+}
+
+func (s ShellDeploymentStrategy) runCommand(c string) error {
+
+	// Create command string
+	cmd := s.createCommand(c)
+	log.Info(" --> Running command: %s", c)
+	// Create command string
+	out, err := cmd.Output()
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Script '%s' with args '%v' not found or is not executable: %v", cmd.Path, cmd.Args, err))
+		return err
+	}
+	log.Info(log.Colorize(log.CYAN, fmt.Sprintf("%s", out)))
+	return nil
+}
+
 func (s ShellDeploymentStrategy) Deploy() error {
 	log.Info("Shell Deploying")
 	for _, c := range s.Commands {
-		log.Info(" --> Running command: %s", c)
+		s.runCommand(c)
 	}
 
 	if rand.Intn(2) == 1 {
@@ -44,7 +69,7 @@ func (s ShellDeploymentStrategy) Deploy() error {
 func (s ShellDeploymentStrategy) Rollback() error {
 	log.Info("Shell Rolling back")
 	for _, c := range s.RollbackCommands {
-		log.Info(" --> Running command: %s", c)
+		s.runCommand(c)
 	}
 	log.Info("Shell Rollback complete!")
 	return nil
